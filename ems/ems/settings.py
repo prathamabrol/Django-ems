2626296c28b9e48a9157876aca1295b42b1ba153
prathamabrol <prathamabrol0@gmail.com>
2024-02-19 14:56:23 +0530

    no chabges

--- ems/ems/settings.py
+++ ems/ems/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-+0et#&zz*v9dvitp7hn&a=tf$fojm@7m94gad=cwg7sm)e%0uh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
